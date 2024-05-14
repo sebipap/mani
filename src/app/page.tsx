79956dbf0b476fb9226eb8510e959b9134de3acb
sebipap <sebipaps@gmail.com>
2024-05-14 10:59:46 -0300

    chore: Add maxDuration constant for page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Expenses from "./components/Expenses";
 
+export const maxDuration = 60;
+
 export default async function Home() {
   /* @ts-expect-error Server Component */
   return <Expenses />;
